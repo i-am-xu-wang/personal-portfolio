b281c52d63d21eed6232a724f1fa663329c1c9b1
Xu Wang <xwang9092@gmail.com>
2023-10-04 16:48:46 -0400

    feat: add resume, github and linkedin cta

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -3,7 +3,10 @@
 import React from "react";
 import Image from "next/image";
 import profile from "../assets/profile.png";
+import resume from "../assets/icon-resume.png";
 import { motion } from "framer-motion";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function intro() {
   return (
@@ -38,7 +41,40 @@ export default function intro() {
           Dive in to see some of the projects I&rsquo;m most proud of.
         </motion.p>
       </div>
-      <div></div>
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <a
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://drive.google.com/file/d/1muAgcQKtp09Q5TiS2cBRiX_QIuQm4l15/view?usp=drive_link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image src={resume} alt="resume-icon" width="33" height="33" />
+        </a>
+        <a
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://www.linkedin.com/in/xu-wang-vt/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsLinkedin size="1.75rem" />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://github.com/i-am-xu-wang"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithubSquare size="1.75rem" />
+        </a>
+      </motion.div>
     </section>
   );
 }
