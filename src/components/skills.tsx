775598f58d74632c7982f3fa7b9d27f4f61c6918
Xu Wang <xwang9092@gmail.com>
2023-10-06 19:28:59 -0400

    feat: add fade in animation increment by index in skills items

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -6,6 +6,15 @@ import { skillsConfig } from "@/lib/data";
 import { useSectionInView } from "@/lib/hook";
 import { motion } from "framer-motion";
 
+const fadeInAnimationVariants = {
+  initial: { opacity: 0, y: 100 },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: { delay: 0.05 * index },
+  }),
+};
+
 export default function Skills() {
   const { ref } = useSectionInView("Skills");
   return (
@@ -16,9 +25,16 @@ export default function Skills() {
     >
       <SectionHeading>Skills</SectionHeading>
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
-        {skillsConfig.map((skill) => (
+        {skillsConfig.map((skill, index) => (
           <motion.li
             key={skill}
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+            custom={index}
             className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
           >
             {skill}
